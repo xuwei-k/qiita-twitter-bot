6880418ad83b587fd9c98718bd5ff9b39ab35d16
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-10-31 23:15:58 +0000

    Reformat with scalafmt 3.6.1
    Executed command: scalafmt --non-interactive

--- config.scala
+++ config.scala
@@ -2,16 +2,15 @@ import qiita_twitter_bot._
 
 import scala.concurrent.duration._
 
-new Config{
+new Config {
   val tag = "scala"
   val interval = 1.hours
   val firstTweetCount = 1
 
-  val twitter = new TwitterSettings{
-    val consumerKey       = ""
-    val consumerSecret    = ""
-    val accessToken       = ""
+  val twitter = new TwitterSettings {
+    val consumerKey = ""
+    val consumerSecret = ""
+    val accessToken = ""
     val accessTokenSecret = ""
   }
 }
-
